2c69efcad6fe007eda9f455ee40563b2795c3012
Evan Yu <92699280+yuevan10284@users.noreply.github.com>
2023-08-18 13:17:13 -0700

    Home screen added w login and signup screen linked

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import PlantDataForm from './PlantDataForm';
+import HomeScreen from './HomeScreen';
+import LoginScreen from './LoginScreen';
+import SignUpScreen from './SignUpScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,7 +13,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+      <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
         <Stack.Screen name="PlantDataForm" component={PlantDataForm} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
