76cfc387bed87d68953745a938476d74a8d98514
Evan Yu <92699280+yuevan10284@users.noreply.github.com>
2023-08-22 15:12:40 -0700

    Add files via upload

--- LoginScreen.js
+++ LoginScreen.js
@@ -1,12 +1,60 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { FIREBASE_AUTH } from './firebaseConfig';
 
-const LoginScreen = () => {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Login Screen</Text>
-    </View>
-  );
+const LoginScreen = ({ navigation }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        try {
+            await signInWithEmailAndPassword(FIREBASE_AUTH, email, password);
+            navigation.navigate('PlantDataForm');
+        } catch (error) {
+            Alert.alert('Error', error.message);
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Login</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Email"
+                value={email}
+                onChangeText={setEmail}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="Password"
+                secureTextEntry
+                value={password}
+                onChangeText={setPassword}
+            />
+            <Button title="Login" onPress={handleLogin} />
+        </View>
+    );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingHorizontal: 20
+    },
+    title: {
+        fontSize: 24,
+        marginBottom: 16,
+        textAlign: 'center'
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 12,
+        paddingHorizontal: 8
+    }
+});
 
 export default LoginScreen;
