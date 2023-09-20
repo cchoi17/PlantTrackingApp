65140f2794909ab0de2da3929170ceb5195171cb
christina choi <112435511+cchoi17@users.noreply.github.com>
2023-09-19 20:20:30 -0700

    Added to QRScanner.js

--- QRScanner.js
+++ QRScanner.js
@@ -1,7 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Text, Button } from 'react-native';
 import { Camera } from 'expo-camera';
+// 'use strict';
 
+// import React, { Component } from 'react';
+
+// import {
+//   AppRegistry,
+//   StyleSheet,
+//   Text,
+//   TouchableOpacity,
+//   Linking
+// } from 'react-native';
+
+// import QRCodeScanner from 'react-native-qrcode-scanner';
+// import { RNCamera } from 'react-native-camera';
+
+// class ScanScreen extends Component {
+//   onSuccess = e => {
+//     Linking.openURL(e.data).catch(err =>
+//       console.error('An error occured', err)
+//     );
+//   };
+
+//   render() {
+//     return (
+//       <QRCodeScanner
+//         onRead={this.onSuccess}
+//         flashMode={RNCamera.Constants.FlashMode.torch}
+//         topContent={
+//           <Text style={styles.centerText}>
+//             Please Scan the QR code.
+//           </Text>
+//         }
+//         bottomContent={
+//           <TouchableOpacity style={styles.buttonTouchable}>
+//             <Text style={styles.buttonText}>OK. Got it!</Text>
+//           </TouchableOpacity>
+//         }
+//       />
+//     );
+//   }
+// }
 function QRScanner({ navigation }) {
   const [hasPermission, setHasPermission] = useState(null);
 
@@ -41,6 +81,16 @@ function QRScanner({ navigation }) {
 }
 
 const styles = StyleSheet.create({
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777'
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000'
+  },
   container: {
     flex: 1
   },
@@ -50,3 +100,4 @@ const styles = StyleSheet.create({
 });
 
 export default QRScanner;
+
