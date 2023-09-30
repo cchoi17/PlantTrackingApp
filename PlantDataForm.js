4f5e46d289ad4d4f6d463d55b1e1fb5dafd691df
Aaron Floreani <aaronfloreani@gmail.com>
2023-09-30 15:37:30 -0700

    added all inputs to plant data form

--- PlantDataForm.js
+++ PlantDataForm.js
@@ -11,32 +11,56 @@ import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { FIRESTORE_DB } from "./firebaseConfig";
 import { useRoute } from "@react-navigation/native";
 import Slider from "@react-native-community/slider";
+import Checkbox from "expo-checkbox";
 
 const PlantDataForm = ({ navigation }) => {
   const [plant, setPlant] = useState("");
+  const [species, setSpecies] = useState("");
   const [isDead, setIsDead] = useState(false);
-  const [plantHealth, setPlantHealth] = useState(5);
+  const [plantHealth, setPlantHealth] = useState("Fair");
+  const [evidenceOf, setEvidenceOf] = useState([]);
 
   const route = useRoute();
+
+  const healthLabels = ["Poor", "Fair", "Good"];
+  const evidenceOptions = [
+    "Predation",
+    "Herbivory",
+    "Flowering",
+    "Seeding",
+    "Drying Out",
+  ];
 
   useEffect(() => {
     // Access the email parameter from the route params
     const { email } = route.params || {};
   }, [route]);
 
+  const toggleEvidenceOption = (option) => {
+    if (evidenceOf.includes(option)) {
+      setEvidenceOf(evidenceOf.filter((value) => value !== option));
+    } else {
+      setEvidenceOf([...evidenceOf, option]);
+    }
+  };
+
   const addPlant = async () => {
     try {
       const docRef = await addDoc(collection(FIRESTORE_DB, "plantsData"), {
         plantID: plant,
+        speciesID: species,
         dead: isDead,
         plantHealth: plantHealth,
+        evidenceOf: evidenceOf,
         email: route.params.email.toLowerCase(),
         timestamp: serverTimestamp(),
       });
       console.log("Added plant with ID: ", docRef.id);
       setPlant("");
+      setSpecies("");
       setIsDead(false);
-      setPlantHealth(5); // Reset all the plant status variables
+      setEvidenceOf([]);
+      setPlantHealth("Fair"); // Reset all the plant status variables
     } catch (error) {
       console.error("Error adding plant: ", error);
     }
@@ -48,9 +72,18 @@ const PlantDataForm = ({ navigation }) => {
         <Text style={styles.formTitles}>Plant ID</Text>
         <TextInput
           style={styles.input}
-          placeholder="Instead of QR code, enter plant ID"
+          placeholder="Enter Plant ID, if QR code is unavailable"
           onChangeText={(text) => setPlant(text)}
           value={plant}
+        />
+      </View>
+      <View style={styles.form}>
+        <Text style={styles.formTitles}>Species</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter Species ID, if QR code is unavailable"
+          onChangeText={(text) => setSpecies(text)}
+          value={species}
         />
       </View>
       <View style={styles.form}>
@@ -68,16 +101,33 @@ const PlantDataForm = ({ navigation }) => {
           <Slider
             style={{ width: 300, height: 50 }}
             minimumValue={0}
-            maximumValue={10}
+            maximumValue={2}
             step={1}
             minimumTrackTintColor="#00cc44"
             maximumTrackTintColor="#006622"
-            thumbTintColor="#00cc44" 
-            value={plantHealth}
-            onValueChange={(value) => setPlantHealth(value)}
+            thumbTintColor="#00cc44"
+            value={healthLabels.indexOf(plantHealth)}
+            onValueChange={(value) => setPlantHealth(healthLabels[value])}
           />
         </View>
       </View>
+<View style={styles.form}>
+  <Text style={styles.formTitles}>Evidence of:</Text>
+  <View style={styles.checkboxContainer}>
+    {evidenceOptions.map((option) => (
+      <View key={option} style={styles.checkboxRow}>
+        <View style={styles.textContainer}>
+          <Text>{option}</Text>
+        </View>
+        <Checkbox
+          value={evidenceOf.includes(option)}
+          onValueChange={() => toggleEvidenceOption(option)}
+        />
+      </View>
+    ))}
+  </View>
+</View>
+
       <Button onPress={addPlant} title="Add Plant" disabled={plant === ""} />
     </View>
   );
@@ -89,6 +139,22 @@ const styles = StyleSheet.create({
   container: {
     marginHorizontal: 20,
   },
+
+  checkboxContainer: {
+    flexDirection: 'column',
+  },
+
+  checkboxRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+    paddingHorizontal: 16, 
+  },
+
+  textContainer: {
+    paddingRight: 40, 
+  },
+
 
   form: {
     flexDirection: "row",
