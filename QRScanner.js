bbdca9f4f85e55957c4d1d4ae1b346e55e6de0d9
christina choi <112435511+cchoi17@users.noreply.github.com>
2023-10-06 15:13:46 -0700

    qr stuff

--- QRScanner.js
+++ QRScanner.js
@@ -1,102 +1,83 @@
-import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
-import { Camera } from 'expo-camera';
-// 'use strict';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import QRCode from 'react-native-qrcode-svg'; 
 
-// import React, { Component } from 'react';
+const QRScanner = () => {
+  const [qrValue, setQrValue] = useState(null);
+  const [showQRCode, setShowQRCode] = useState(false); // New state to toggle showing the QR code
 
-// import {
-//   AppRegistry,
-//   StyleSheet,
-//   Text,
-//   TouchableOpacity,
-//   Linking
-// } from 'react-native';
+  const handleBarCodeScanned = ({ type, data }) => {
+    setQrValue(data);
 
-// import QRCodeScanner from 'react-native-qrcode-scanner';
-// import { RNCamera } from 'react-native-camera';
+    // Attempt to open the URL
+    if (data) {
+        Linking.canOpenURL(data)
+            .then(supported => {
+                if (supported) {
+                    Linking.openURL(data);
+                } else {
+                    console.log("Don't know how to open URI: " + data);
+                }
+            })
+            .catch(err => console.error("An error occurred", err));
+    }
+};
 
-// class ScanScreen extends Component {
-//   onSuccess = e => {
-//     Linking.openURL(e.data).catch(err =>
-//       console.error('An error occured', err)
-//     );
-//   };
 
-//   render() {
-//     return (
-//       <QRCodeScanner
-//         onRead={this.onSuccess}
-//         flashMode={RNCamera.Constants.FlashMode.torch}
-//         topContent={
-//           <Text style={styles.centerText}>
-//             Please Scan the QR code.
-//           </Text>
-//         }
-//         bottomContent={
-//           <TouchableOpacity style={styles.buttonTouchable}>
-//             <Text style={styles.buttonText}>OK. Got it!</Text>
-//           </TouchableOpacity>
-//         }
-//       />
-//     );
-//   }
-// }
-function QRScanner({ navigation }) {
-  const [hasPermission, setHasPermission] = useState(null);
-
-  const requestCameraPermission = async () => {
-    const { status } = await Camera.requestPermissionsAsync();
-    setHasPermission(status === 'granted');
+  const handleGenerateQRCode = () => {
+    setQrValue('yourapp://path/to/page'); // Set your deep link here
+    setShowQRCode(true); // Show the QR code
   };
-
-  useEffect(() => {
-    requestCameraPermission();
-  }, []);
-
-  const handleScan = ({ type, data }) => {
-    navigation.navigate('Input', { qrData: data });
-  };
-
-  if (hasPermission === null) {
-    return <Text>Requesting camera permission...</Text>;
-  }
-  if (hasPermission === false) {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>No access to camera</Text>
-        <Button title="Retry Permission" onPress={requestCameraPermission} />
-      </View>
-    );
-  }
 
   return (
     <View style={styles.container}>
-      <Camera 
-        style={styles.camera} 
-        onBarCodeScanned={handleScan}
-      />
+      {showQRCode ? ( // If showQRCode is true, display the QR code
+        <View style={styles.qrCodeContainer}>
+          <QRCode value={qrValue} size={200} />
+          <Text style={styles.qrValue}>
+            {qrValue ? `QR Code Value: ${qrValue}` : ''}
+          </Text>
+        </View>
+      ) : (
+        <RNCamera
+          style={styles.camera}
+          type={RNCamera.Constants.Type.back}
+          captureAudio={false}
+          barCodeTypes={[RNCamera.Constants.BarCodeType.qr]}
+          onBarCodeRead={handleBarCodeScanned}
+        >
+          <Text style={styles.qrValue}>
+            {qrValue ? `Scanned QR Code Value: ${qrValue}` : 'Scan a QR Code'}
+          </Text>
+          <Button title="Generate QR Code" onPress={handleGenerateQRCode} /> {/* Button to generate QR code */}
+        </RNCamera>
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  centerText: {
-    flex: 1,
-    fontSize: 18,
-    padding: 32,
-    color: '#777'
-  },
-  textBold: {
-    fontWeight: '500',
-    color: '#000'
-  },
   container: {
-    flex: 1
+    flex: 1,
   },
   camera: {
-    flex: 1
-  }
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    padding: 20,
+  },
+  qrCodeContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  qrValue: {
+    fontSize: 18,
+    marginBottom: 10,
+    color: 'white',
+  },
 });
 
 export default QRScanner;
