49125be5a8ca993c1a1227ff1403514f56870108
Evan Yu <92699280+yuevan10284@users.noreply.github.com>
2023-11-30 13:10:37 -0800

    Add files via upload

--- PlantDataForm.js
+++ PlantDataForm.js
@@ -5,6 +5,7 @@ import {
   StyleSheet,
   TextInput,
   Switch,
+  ScrollView,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
@@ -15,12 +16,12 @@ import Checkbox from "expo-checkbox";
 
 const PlantDataForm = ({ navigation }) => {
   const [plant, setPlant] = useState("");
-  const [species, setSpecies] = useState("Waiting on scan...");
+  const [species, setSpecies] = useState("");
   const [isDead, setIsDead] = useState(false);
   const [plantHealth, setPlantHealth] = useState("Fair");
   const [evidenceOf, setEvidenceOf] = useState([]);
-  const [userEmail, setUserEmail] = useState("Email not found");
-  const [scannedData, setScannedData] = useState("Waiting on scan...");
+  const [userEmail, setUserEmail] = useState("");
+  const [scannedData, setScannedData] = useState("");
 
   const route = useRoute();
 
@@ -53,18 +54,21 @@ const PlantDataForm = ({ navigation }) => {
   const prefix = scannedData.substring(0, 4);
 
   // Use the mappingRules to get the corresponding species
-  const species = mappingRules[prefix] || "Unknown species";
+  const species = mappingRules[prefix] || "";
 
   return species;
 };
 
-    useEffect(() => {
-        const { scannedData = "Waiting on scan...", email = "" } = route.params ?? {};
-        setScannedData(scannedData || "Waiting on scan...");
-        setUserEmail(email || "email not found");
-        setSpecies(mapScannedDataToSpecies(scannedData) || "Waiting on scan...");
+  useEffect(() => {
+    const { scannedData = "", email = "" } = route.params ?? {};
+    if (scannedData) {
+      setPlant(scannedData);
+      setSpecies(mapScannedDataToSpecies(scannedData));
     }
-    , [route]);
+    if (email) {
+      setUserEmail(email);
+    }
+  }, [route.params]);
 
 
 
@@ -95,120 +99,168 @@ const PlantDataForm = ({ navigation }) => {
       setIsDead(false);
       setEvidenceOf([]);
       setPlantHealth("Fair"); // Reset all the plant status variables
-      setScannedData("Waiting on scan...")
+      setScannedData("")
     } catch (error) {
-      console.error("Error adding plant: ", error);
+      console.error("", error);
     }
   };
 
   return (
-    <View style={styles.container}>
-      <View>
+    <ScrollView style={styles.container}>
+      <Text style={styles.header}>PlantDataForm</Text>
+      <View style={styles.buttonContainer}>
         <Button
           title="Go to QR Code Scanner"
-          onPress={() => navigation.navigate('QRCodeScanner', { email: userEmail})}
+          onPress={() => navigation.navigate('QRCodeScanner')}
+          color="#FFFFFF"
         />
       </View>
-      <View style={styles.form}>
-        <Text style={styles.formTitles}>Plant ID:</Text>
-        <Text style={styles.formTitles}>{scannedData}</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          onChangeText={setPlant}
+          value={plant}
+          placeholder="Enter Plant ID if QR Code is unavailable"
+          placeholderTextColor="#FFF" // White placeholder text
+        />
       </View>
-      <View style={styles.form}>
-        <Text style={styles.formTitles}>Species</Text>
-        <Text style={styles.formTitles}>{species}</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          onChangeText={setSpecies}
+          value={species}
+          placeholder="Enter Species ID if QR Code is unavailable"
+          placeholderTextColor="#FFF" // White placeholder text
+        />
       </View>
-      <View style={styles.form}>
-        <Text style={styles.formTitles}>Dead</Text>
+      <View style={styles.switchContainer}>
+        <Text style={styles.switchLabel}>Dead</Text>
         <Switch
-          trackColor={{ false: "#767577", true: "#00cc44" }}
-          thumbColor={isDead ? "#ccffdd" : "#f4f3f4"}
-          onValueChange={(value) => setIsDead(value)}
+          trackColor={{ false: "#D1D1D6", true: "#34C759" }}
+          thumbColor="#FFFFFF"
+          onValueChange={setIsDead}
           value={isDead}
         />
       </View>
+      <View style={styles.sliderContainer}>
+        <Text style={styles.switchLabel}>Plant Health: {plantHealth}</Text>
+        <Slider
+          style={styles.slider}
+          minimumValue={0}
+          maximumValue={2}
+          step={1}
+          minimumTrackTintColor="#34C759"
+          maximumTrackTintColor="#C7C7CC"
+          thumbTintColor="#34C759"
+          value={healthLabels.indexOf(plantHealth)}
+          onValueChange={(value) => setPlantHealth(healthLabels[value])}
+        />
+      </View>
       <View>
-        <Text style={styles.formTitles}>Plant Health: {plantHealth}</Text>
-        <View style={{ alignItems: "center" }}>
-          <Slider
-            style={{ width: 300, height: 50 }}
-            minimumValue={0}
-            maximumValue={2}
-            step={1}
-            minimumTrackTintColor="#00cc44"
-            maximumTrackTintColor="#006622"
-            thumbTintColor="#00cc44"
-            value={healthLabels.indexOf(plantHealth)}
-            onValueChange={(value) => setPlantHealth(healthLabels[value])}
-          />
-        </View>
+        <Text style={styles.switchLabel}>Evidence of:</Text>
+        {evidenceOptions.map((option) => (
+          <View key={option} style={styles.checkboxContainer}>
+            <Checkbox
+              value={evidenceOf.includes(option)}
+              onValueChange={() => toggleEvidenceOption(option)}
+              style={styles.checkbox}
+            />
+            <Text style={styles.checkboxLabel}>{option}</Text>
+          </View>
+        ))}
       </View>
-      <View style={styles.form}>
-        <Text style={styles.formTitles}>Evidence of:</Text>
-        <View style={styles.checkboxContainer}>
-          {evidenceOptions.map((option) => (
-            <View key={option} style={styles.checkboxRow}>
-              <View style={styles.textContainer}>
-                <Text>{option}</Text>
-              </View>
-              <Checkbox
-                value={evidenceOf.includes(option)}
-                onValueChange={() => toggleEvidenceOption(option)}
-                style={styles.checkbox}
-              />
-            </View>
-          ))}
-        </View>
+      <View style={styles.buttonContainer}>
+        <Button
+          onPress={addPlant}
+          title="Add Plant"
+          color="#FFFFFF"
+          disabled={plant === ""}
+        />
       </View>
-
-      <Button onPress={addPlant} title="Add Plant" disabled={scannedData === "Waiting on scan..."} />
-    </View>
+    </ScrollView>
   );
 };
 
-export default PlantDataForm;
-
 const styles = StyleSheet.create({
   container: {
-    marginHorizontal: 20,
-  },
-
-  checkboxContainer: {
-    flexDirection: "column",
-  },
-
-  checkboxRow: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginBottom: 10,
-    paddingHorizontal: 16,
-  },
-
-    checkbox: {
-    width: 30, 
-    height: 30, 
-  },
-
-  textContainer: {
-    paddingRight: 40,
-  },
-
-  form: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginVertical: 20,
-  },
-
-  formTitles: {
-    marginRight: 10,
-  },
-
-  input: {
-    borderWidth: 1,
-    borderColor: "#2F5233",
-    backgroundColor: "#e6ffe6",
-    height: 40,
-    borderRadius: 4,
     flex: 1,
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingVertical: 20,
+  },
+  inputContainer: {
+    marginBottom: 20,
+  },
+  input: {
+    backgroundColor: '#D2B48C', // Light brown background
+    color: 'white', // White text
+    borderRadius: 10,
+    padding: 15,
+    fontSize: 16,
+  },
+  buttonContainer: {
+    backgroundColor: '#34C759',
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  labelContainer: {
+    backgroundColor: '#D2B48C',
+    borderRadius: 10,
+    padding: 15,
+    marginBottom: 20,
+  },
+  labelText: {
+    fontSize: 16,
+    color: 'white'
+  },
+  switchContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+  switchLabel: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  sliderContainer: {
+    alignItems: 'stretch',
+    marginBottom: 20,
+  },
+  slider: {
+    width: '100%',
+    height: 40,
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  checkboxLabel: {
+    flex: 1,
+    marginLeft: 10,
+    fontSize: 16,
+  },
+  checkbox: {
+    alignSelf: 'center',
+  },
+  addButton: {
+    backgroundColor: '#1B5E20',
+    borderRadius: 10,
     padding: 10,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  addButtonText: {
+    color: 'white',
+    fontWeight: '600',
+    fontSize: 18,
   },
 });
+
+export default PlantDataForm;
